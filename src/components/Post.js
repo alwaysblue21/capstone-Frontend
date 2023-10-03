2c56c43f69b9480d507cc8580582c722fbd8aa72
Je Yun <alwaysblue21@gmail.com>
2023-10-02 22:05:05 -0400

    some post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,22 @@
-function Post (props){
-    return <div>Post</div>
+import {Link} from 'react-router-dom';
+
+// describe the post prop
+function Post ({post}){
+
+    const div = {
+        textAlign: 'center',
+        border: `3px solid`,
+        margin: "10px auto",
+        width: "80%"
+    }
+
+
+    return <div style={div}>
+        <Link to={`/post/${post.id}`}>
+            <h1>{post.name}</h1>
+            <img src={post.image} alt={post.name} />
+        </Link>
+    </div>
 }
 
 export default Post;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,14 @@
+import Post from '../components/Post';
+import {useLoaderData} from 'react-router-dom';
+
 function Index (props){
-    return <div>Index</div>
+
+    // fetch the loaderdata using the useLoaderData hook
+    const legos = useLoaderData()
+
+    // map over the legos and create a Post component for each lego
+    return legos.map((lego) => <Post key={lego.id} post={lego}/>)
+
 }
 
 export default Index;
