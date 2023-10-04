40757204242fd23bed7ce99eedb62c17506d6a46
Je Yun <alwaysblue21@gmail.com>
2023-10-04 19:05:38 -0400

    show.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,5 @@
 import Post from '../components/Post';
-import {useLoaderData} from 'react-router-dom';
+import {useLoaderData, Form} from 'react-router-dom';
 
 function Index (props){
 
@@ -7,7 +7,21 @@ function Index (props){
     const legos = useLoaderData()
 
     // map over the legos and create a Post component for each lego
-    return legos.map((lego) => <Post key={lego.id} post={lego}/>)
+    return <>
+    <div style={{textAlign: "center"}}>
+        <h2>Add A New Lego</h2>
+        <Form method="post" action="/create">
+            <input type="text" name="name" placeholder="Name"/>
+            <input type="text" name="image" placeholder="Image Url"/>
+            <input type="text" name="age" placeholder="Recommended Age"/>
+            <input type="text" name="pieces" placeholder="Pieces"/>
+            <input type="text" name="item_number" placeholder="Model Number"/>
+            <input type="text" name="price" placeholder="Price"/>
+            <button>Create a new Lego</button>
+        </Form>
+    </div>
+    {legos.map((lego) => <Post key={lego.id} post={lego}/>)}
+    </>
 
 }
 
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,19 +1,61 @@
-import {Link, useLoaderData} from 'react-router-dom';
+import { Link, useLoaderData, Form } from "react-router-dom";
 
-function Show (props){
-    const post = useLoaderData()
+function Show(props) {
+    const post = useLoaderData();
 
     return (
-    <div >
-        <h1>Model Name: {post.name}</h1>
-        <img src={post.image} alt={post.name} />
-        <h2>Recommended Age: {post.age}</h2>
-        <h2>Pieces: {post.pieces}</h2>
-        <h2>Model Number: {post.item_number}</h2>
-        <h2>Price: ${post.price}</h2>
+        <div>
+            <h1>Model Name: {post.name}</h1>
+            <img src={post.image} alt={post.name} />
+            <h2>Recommended Age: {post.age}</h2>
+            <h2>Pieces: {post.pieces}</h2>
+            <h2>Model Number: {post.item_number}</h2>
+            <h2>Price: ${post.price}</h2>
+        <div style={{ textAlign: "center" }}>
+        <h2>Update Lego</h2>
+        <Form method="post" action={`/update/${post.id}`}>
+        <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            defaultValue={post.name}
+        />
+        <input
+            type="text"
+            name="image"
+            placeholder="Image Url"
+            defaultValue={post.image}
+        />
+        <input
+            type="text"
+            name="age"
+            placeholder="Age"
+            defaultValue={post.age}
+        />
+        <input
+            type="text"
+            name="pieces"
+            placeholder="Pieces"
+            defaultValue={post.pieces}
+        />
+        <input
+            type="text"
+            name="item_number"
+            placeholder="Model Number"
+            defaultValue={post.item_number}
+        />
+        <input
+            type="text"
+            name="price"
+            placeholder="Price"
+            defaultValue={post.price}
+        />
+        <button>Update Lego</button>
+        </Form>
+        </div>
         <Link to="/">Back to Index</Link>
-    </div>
-    )
+        </div>
+    );
 }
 
 export default Show;
