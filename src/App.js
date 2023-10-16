db4fd4b2500099fc4b8c9bfdb9c982e8b678a629
Je Yun <alwaysblue21@gmail.com>
2023-10-16 18:13:19 -0400

    title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import {Outlet} from 'react-router-dom';
 import './App.css';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+                <meta charSet="utf-8" />
+                <title>Capstone Project</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+                <meta name="description" content="Helmet application" />
+            </Helmet>
       <Outlet/>
     </div>
   );
